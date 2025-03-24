8956eb4222fab84c068173b33f883b14e7e58134
Indis Yeromina <i.yeromina@inno-cube.at>
2025-03-24 18:13:18 +0100

    upload all files

---
+++ src/components/CityItem.jsx
@@ -0,0 +1,40 @@
+import { useCities } from "../contexts/CitiesContext";
+import styles from "./CityItem.module.css";
+import { Link } from "react-router-dom";
+
+const formatDate = (date) =>
+  new Intl.DateTimeFormat("en", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  }).format(new Date(date));
+
+function CityItem({ city }) {
+  const { cityName, date, id, position } = city;
+  const { currentCity, deleteCity } = useCities();
+
+  async function handleClick(e) {
+    e.preventDefault();
+
+    await deleteCity(id);
+  }
+
+  return (
+    <li>
+      <Link
+        className={`${styles.cityItem} ${
+          id === currentCity.id ? styles["cityItem--active"] : ""
+        }`}
+        to={`${id}?lat=${position.lat}&lng=${position.lng}`}
+      >
+        <h3 className={styles.name}>{cityName}</h3>
+        <time className={styles.date}>({formatDate(date)})</time>
+        <button className={styles.deleteBtn} onClick={handleClick}>
+          &times;
+        </button>
+      </Link>
+    </li>
+  );
+}
+
+export default CityItem;
